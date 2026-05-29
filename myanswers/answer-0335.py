4c7a0cd248e0ebf94731d3842f1b904a4afda497
TheMax1270 <mdaniel.perez@udea.edu.co>
2026-05-29 13:23:12 -0500

    Update answer-0335.py

--- myanswers/answer-0335.py
+++ myanswers/answer-0335.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
 
+def entrenar_modelo(df, target_col):
+    X = df.drop(columns=[target_col])
+    y = df[target_col]
+
+    model = LogisticRegression()
+    model.fit(X, y)
+
+    return model
