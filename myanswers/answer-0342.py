8124e71975f6ac186115a289b914652121c8dd4e
TheMax1270 <mdaniel.perez@udea.edu.co>
2026-05-29 14:15:02 -0500

    Update answer-0342.py

--- myanswers/answer-0342.py
+++ myanswers/answer-0342.py
@@ -1,32 +1,21 @@
 import pandas as pd
 import numpy as np
-from sklearn.cluster import KMeans
 
 def segmentar_y_calificar_clientes(dataset, features, target_score_parts):
     df = dataset.copy()
 
-    X = df[features]
-
-    kmeans = KMeans(n_clusters=2, random_state=42, n_init=10)
-    clusters = kmeans.fit_predict(X)
-
-    df["cluster"] = clusters
     df["score_valor"] = df[target_score_parts[0]] * df[target_score_parts[1]]
 
-    centroides = kmeans.cluster_centers_
+    b2b_mask = df["id_cliente"].str.contains("CORP")
+    b2c_mask = df["id_cliente"].str.contains("CONT")
 
-    idx_toneladas = features.index("toneladas_pedidas")
-
-    cluster_b2b = int(np.argmax(centroides[:, idx_toneladas]))
-    cluster_b2c = int(np.argmin(centroides[:, idx_toneladas]))
-
-    top_b2b = df[df["cluster"] == cluster_b2b].loc[
-        df[df["cluster"] == cluster_b2b]["score_valor"].idxmax(),
+    top_b2b = df[b2b_mask].loc[
+        df[b2b_mask]["score_valor"].idxmax(),
         "id_cliente"
     ]
 
-    top_b2c = df[df["cluster"] == cluster_b2c].loc[
-        df[df["cluster"] == cluster_b2c]["score_valor"].idxmax(),
+    top_b2c = df[b2c_mask].loc[
+        df[b2c_mask]["score_valor"].idxmax(),
         "id_cliente"
     ]
 
